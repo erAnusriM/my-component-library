80f9407c9315123470ae6da8baf22d11e7bcd9ab
Anusri M <anusri.mukherjee20@gmail.com>
2022-01-13 20:23:39 +0530

    my component changes

---
+++ my-component-library/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class',
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './stories/*'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
